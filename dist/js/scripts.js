3cc5868d7f916c4f01bcd58d0c066c8246e12873
Sean Brodbeck <sean@seanbrodbeck.com>
2017-07-10 13:00:48 -0400

    First commit

---
+++ dist/js/scripts.js
@@ -0,0 +1,34 @@
+(function($) {
+
+  // Header Shrink
+  function init() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 100,
+            header = document.querySelector("header");
+        if (distanceY > shrinkOn) {
+            classie.add(header,"smaller");
+        } else {
+            if (classie.has(header,"smaller")) {
+                classie.remove(header,"smaller");
+            }
+        }
+    });
+  }
+  window.onload = init();
+
+  // Flex Slider
+
+  $('.flexslider').flexslider({
+    controlNav: false,
+    prevText: "",
+    nextText: "" 
+  });
+
+  // Add Active Class to Tabs on Dermatology Pages
+
+  $( "#dermatology-tabs .nav-tabs li:first-child" ).addClass( "active" );
+  $( "#dermatology-tabs .tab-content .tab-pane:first-child" ).addClass( "active in" );
+ 
+})( jQuery );
+
