c6e691043504f210f8bed99c338bbc8a5332de0d
narrowdesign <nick@narrowdesign.com>
2017-07-28 09:23:03 -0400

    add(header): sticky

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -27,10 +27,23 @@ function animateTransition() {
   let cancelAnimationFrame = window.cancelAnimationFrame;
   let scrollRequest;
   let qScrollPositions = [];
+  let isActive = false;
 
-  let init = () => {
+  let initIntroScript = () => {
     Array.prototype.map.call(questions, (q, i) => {
-      if (i < questions.length) addImageHover(i);
+      if (i < questions.length) {
+        addImageHover(i);
+        addTextClick(i);
+      }
+    })
+    document.addEventListener('wheel',navScrollHandler)
+    window.addEventListener('click', () => {
+      isActive = false;
+      Array.prototype.map.call(images, (image, i) => {
+        images[i].classList.remove('is-active');
+        questions[i].classList.remove('is-active')
+        questions[i].classList.remove('is-off')
+      })
     })
   }
 
@@ -38,30 +51,44 @@ function animateTransition() {
     let testVar = 'whatever';
     qScrollPositions[i] = 0;
     questions[i].addEventListener('mouseenter', () => {
-      images[i].classList.add('is-active');
       scrollQuestionText(i)
-      Array.prototype.map.call(questions, (q, j) => {
-        if (i != j) {
-          questions[j].classList.add('is-off');
-        }
-      })
     })
     questions[i].addEventListener('mouseleave', () => {
-      images[i].classList.remove('is-active');
       cancelAnimationFrame(scrollRequest);
-      Array.prototype.map.call(questions, (q, j) => {
-        questions[j].classList.remove('is-active')
-        questions[j].classList.remove('is-off')
-      })
+    })
+  }
+
+  let addTextClick = i => {
+    questions[i].addEventListener('click', (e) => {
+      if (!isActive) {
+        e.stopPropagation()
+        isActive = true;
+        images[i].classList.add('is-active');
+        Array.prototype.map.call(questions, (q, j) => {
+          if (i != j) {
+            questions[j].classList.add('is-off');
+          } else {
+            questions[j].classList.add('is-active');
+          }
+        })
+      }
     })
   }
 
   let scrollQuestionText = i => {
-    qScrollPositions[i] = qScrollPositions[i] - 8
+    qScrollPositions[i] = qScrollPositions[i] - 5
     questions[i].querySelector('.intro-question-text').style.transform = 'translate3d(' + qScrollPositions[i] + 'px,0,0)'
     scrollRequest = requestAnimationFrame( () => { scrollQuestionText(i)})
   }
-  init();
+  let navScrollHandler = (e) => {
+    if (document.querySelector('#masthead').getBoundingClientRect().bottom < 75) {
+      document.body.classList.add('is-nav-fixed')
+    } else {
+      document.body.classList.remove('is-nav-fixed')
+    }
+  }
+
+  initIntroScript();
 
   // Toggle Search and Filter
   $( ".filter-toggle" ).click(function() {
@@ -88,13 +115,13 @@ function animateTransition() {
         $( this ).addClass('is-checked');
       });
     });
-
-
   });
+
+  // FILTER SEARCH
   $( ".filter-search-btn" ).click(function() {
     $( "#search-overlay" ).fadeIn( "fast", "linear" );
     $(".quicksearch").focus();
-    // Isotope Search 
+    // Isotope Search
 
       var qsRegex;
 
@@ -143,6 +170,7 @@ var WIN_H = window.innerHeight,
     el_primary_list,
     el_secondary_list,
     scrollHeight,
+    scrollTop,
     scrollPercent,
     primaryHeight,
     secondaryHeight,
@@ -150,37 +178,39 @@ var WIN_H = window.innerHeight,
     newY,
     targY;
 
-var detectRenderInterval = setInterval(function(){
-  initScroll();
-},300);
+if (document.querySelector('.blog .primary-articles')) {
+  var detectRenderInterval = setInterval(function(){
+    initScroll();
+  },300);
 
-function initScroll() {
-  clearInterval(detectRenderInterval);
-  el_primary_list = document.querySelector('.blog .primary-articles');
-  el_secondary_list = document.querySelector('.blog .secondary-articles');
-  document.addEventListener('wheel',scrollHandler)
-  scrollHeight = document.body.offsetHeight - WIN_H;
-  primaryHeight = el_primary_list.offsetHeight;
-  secondaryHeight = el_secondary_list.offsetHeight;
-  heightDifference = primaryHeight - secondaryHeight;
-  secondaryTargY = scrollHeight - heightDifference
-  oldY = 0;
-  newY = 0;
-  targY = 0;
-}
-function scrollHandler(e) {
-  requestAnimationFrame(function(){
-    var scrollTop = document.body.scrollTop;
-    scrollPercent = scrollTop/scrollHeight;
-    if (primaryHeight > secondaryHeight) {
-      el_secondary_list.style.willChange = "transform";
-      el_secondary_list.style.transform = 'translate3d(0, ' + Math.floor(-secondaryTargY*scrollPercent) + 'px, 0)'
-    }
-  })
-}
-function setSecondaryY(secondaryScrollHeight, scrollPercent) {
-  targY = Math.floor(secondaryScrollHeight * scrollPercent);
-  newY += .9*(-targY - newY); // Slightly eased
-  return newY;
-}
+  function initScroll() {
+    clearInterval(detectRenderInterval);
+    el_primary_list = document.querySelector('.blog .primary-articles');
+    el_secondary_list = document.querySelector('.blog .secondary-articles');
+    document.addEventListener('wheel',articlesScrollHandler)
+    scrollHeight = document.body.offsetHeight - WIN_H;
+    primaryHeight = el_primary_list.offsetHeight;
+    secondaryHeight = el_secondary_list.offsetHeight;
+    heightDifference = primaryHeight - secondaryHeight;
+    secondaryTargY = scrollHeight - heightDifference
+    oldY = 0;
+    newY = 0;
+    targY = 0;
+  }
+  function articlesScrollHandler(e) {
+    requestAnimationFrame(function(){
+      scrollTop = document.body.scrollTop; // REDUNDANT
+      scrollPercent = scrollTop/scrollHeight;
+      if (primaryHeight > secondaryHeight) {
+        el_secondary_list.style.willChange = "transform";
+        el_secondary_list.style.transform = 'translate3d(0, ' + Math.floor(-secondaryTargY*scrollPercent) + 'px, 0)'
+      }
+    })
+  }
+  function setSecondaryY(secondaryScrollHeight, scrollPercent) {
+    targY = Math.floor(secondaryScrollHeight * scrollPercent);
+    newY += .9*(-targY - newY); // Slightly eased
+    return newY;
+  }
 
+}
