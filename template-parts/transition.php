8ea1e31894ec3f136d9eefe5d95b312f2b137c64
narrowdesign <nick@narrowdesign.com>
2017-08-01 22:41:50 -0400

    update(intro/transtiion): heights

--- template-parts/transition.php
+++ template-parts/transition.php
@@ -7,4 +7,6 @@
   <h1><span>A reflection of you A reflection of you A reflection of you</span></h1>
   <h1><span>A reflection of you A reflection of you A reflection of you</span></h1>
   <h1><span>A reflection of you A reflection of you A reflection of you</span></h1>
+  <h1><span>A reflection of you A reflection of you A reflection of you</span></h1>
+  <h1><span>A reflection of you A reflection of you A reflection of you</span></h1>
 </div>
