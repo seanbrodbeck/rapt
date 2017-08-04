3ceac41195abaaf5e299a8325c63552b49321055
narrowdesign <nick@narrowdesign.com>
2017-08-03 22:59:45 -0400

    update(alignments): better

--- page-things-landing.php
+++ page-things-landing.php
@@ -26,7 +26,7 @@ get_header(); ?>
 				</div>
 			</div>
 
-				<div class="work-grid">
+			<div class="work-grid">
 
 				<?php
 					if (have_posts()) : while (have_posts()) : the_post();
@@ -52,76 +52,76 @@ get_header(); ?>
 
 					endwhile; endif; ?>
 
+			</div>
+
+			<div id="filter-overlay" class="search-filter-overlay" style="display:none">
+				<div class="full-width-wrap">
+					<img class="close-filter-overlay" src="/wp-content/themes/rapt/dist/images/close.svg" width="32" height="auto"/>
+					<div class="col-sm-10">
+						<div class="filter-options">
+
+							<?php
+
+							$taxonomy = 'product_categories';
+							$terms = get_terms($taxonomy);
+
+							if ( $terms && !is_wp_error( $terms ) ) :
+							?>
+
+				        <?php foreach ( $terms as $term ) { ?>
+				            <span class="filter-option" data-filter=".<?php echo $term->slug; ?>"><?php echo $term->name; ?></span>
+				        <?php } ?>
+
+							<?php endif;?>
+
+							<?php wp_reset_postdata();?>
+
+						</div>
+					</div>
 				</div>
+				<div class="container">
+					<div class="col-sm-12">
+						<div class="row filter-listings filter-listings-filters clearfix">
+							<?php
 
-				<div id="filter-overlay" class="search-filter-overlay" style="display:none">
-			<div class="full-width-wrap">
-				<img class="close-filter-overlay" src="/wp-content/themes/rapt/dist/images/close.svg" width="32" height="auto"/>
-				<div class="col-sm-10">
-					<div class="filter-options">
+								global $post;
+								$args = array( 'posts_per_page' => -1, 'order'=> 'ASC', 'orderby' => 'date', 'post_type' => 'things_post_type' );
 
-						<?php
+								$myposts = get_posts( $args );
+								foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
 
-						$taxonomy = 'product_categories';
-						$terms = get_terms($taxonomy);
+								<?php $terms = get_the_terms( get_the_ID(), 'product_categories' );
 
-						if ( $terms && !is_wp_error( $terms ) ) :
-						?>
+	              if ( $terms && ! is_wp_error( $terms ) ) :
 
-			        <?php foreach ( $terms as $term ) { ?>
-			            <span class="filter-option" data-filter=".<?php echo $term->slug; ?>"><?php echo $term->name; ?></span>
-			        <?php } ?>
+	                $work_cat_list = array();
+	              	$work_cat_slug = array();
 
-						<?php endif;?>
+	                foreach ( $terms as $term ) {
+	                    $work_cat_list[] = $term->name;
+	                    $work_cat_slug[] = $term->slug;
+	                }
 
-						<?php wp_reset_postdata();?>
+	                $work_cats = join( " · ", $work_cat_list );
+	                $work_cats_slugs = join( " ", $work_cat_slug );
 
+	                ?>
+									<div class="col-md-4 col-sm-6 col-xs-12 filter-listing filter-listing-filter <?php printf( esc_html__( '%s', 'textdomain' ), esc_html( $work_cats_slugs ) ); ?>" data-category="<?php printf( esc_html__( '%s', 'textdomain' ), esc_html( $work_cats_slugs ) ); ?>">
+							 				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('filter-thumb'); ?></a>
+							 				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			                <p class="work-cats category-list"><?php printf( esc_html__( '%s', 'textdomain' ), esc_html( $work_cats ) ); ?></p>
+
+				              <?php endif; ?>
+								 	</div>
+								<?php endforeach;
+								wp_reset_postdata();?>
+								<div class="clearfix"></div>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div class="container">
-				<div class="col-sm-12">
-					<div class="row filter-listings filter-listings-filters clearfix">
-						<?php
 
-							global $post;
-							$args = array( 'posts_per_page' => -1, 'order'=> 'ASC', 'orderby' => 'date', 'post_type' => 'things_post_type' );
-
-							$myposts = get_posts( $args );
-							foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-
-							<?php $terms = get_the_terms( get_the_ID(), 'product_categories' );
-
-              if ( $terms && ! is_wp_error( $terms ) ) :
-
-                $work_cat_list = array();
-              	$work_cat_slug = array();
-
-                foreach ( $terms as $term ) {
-                    $work_cat_list[] = $term->name;
-                    $work_cat_slug[] = $term->slug;
-                }
-
-                $work_cats = join( " · ", $work_cat_list );
-                $work_cats_slugs = join( " ", $work_cat_slug );
-
-                ?>
-								<div class="col-md-4 col-sm-6 col-xs-12 filter-listing filter-listing-filter <?php printf( esc_html__( '%s', 'textdomain' ), esc_html( $work_cats_slugs ) ); ?>" data-category="<?php printf( esc_html__( '%s', 'textdomain' ), esc_html( $work_cats_slugs ) ); ?>">
-						 				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('filter-thumb'); ?></a>
-						 				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-		                <p class="work-cats category-list"><?php printf( esc_html__( '%s', 'textdomain' ), esc_html( $work_cats ) ); ?></p>
-
-			              <?php endif; ?>
-							 	</div>
-							<?php endforeach;
-							wp_reset_postdata();?>
-							<div class="clearfix"></div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-			<div id="search-overlay" class="search-filter-overlay" style="display:none;">
+		<div id="search-overlay" class="search-filter-overlay" style="display:none;">
 			<div class="full-width-wrap">
 				<img class="close-search-overlay" src="/wp-content/themes/rapt/dist/images/close.svg" width="32" height="auto"/>
 				<input type="text" class="quicksearch" placeholder="Start Typing...">
