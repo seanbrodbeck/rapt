ab120c55af64e6486de1b3b3b64dd574318287c4
narrowdesign <nick@narrowdesign.com>
2017-08-09 19:21:01 -0400

    update(compression): dont compress images

--- functions.php
+++ functions.php
@@ -48,6 +48,7 @@ function rapt_setup() {
 	) );
 
 	add_image_size( 'filter-thumb', 300, 160, true ); // 300 pixels wide
+	add_filter('jpeg_quality', function($arg){return 100;});
 
 	/*
 	 * Switch default core markup for search form, comment form, and comments
@@ -149,15 +150,15 @@ add_action( 'wp_enqueue_scripts', 'rapt_scripts' );
 // Load More Enqueue
 
 function misha_my_load_more_scripts() {
- 
-	global $wp_query; 
- 
+
+	global $wp_query;
+
 	// In most cases it is already included on the page and this line can be removed
 	wp_enqueue_script('jquery');
- 
+
 	// register our main script but do not enqueue it yet
 	wp_register_script( 'my_loadmore', get_template_directory_uri() . '/dist/js/myloadmore.js', array('jquery') );
- 
+
 	// now the most interesting part
 	// we have to pass parameters to myloadmore.js script but we can get the parameters values only in PHP
 	// you can define variables directly in your HTML but I decided that the most proper way is wp_localize_script()
@@ -167,39 +168,39 @@ function misha_my_load_more_scripts() {
 		'current_page' => get_query_var( 'paged' ) ? get_query_var('paged') : 1,
 		'max_page' => $wp_query->max_num_pages
 	) );
- 
+
  	wp_enqueue_script( 'my_loadmore' );
 }
- 
+
 add_action( 'wp_enqueue_scripts', 'misha_my_load_more_scripts' );
 
 
 function misha_loadmore_ajax_handler(){
- 
+
 	// prepare our arguments for the query
 	$args = unserialize( stripslashes( $_POST['query'] ) );
 	$args['paged'] = $_POST['page'] + 1; // we need next page to be loaded
 	$args['post_status'] = 'publish';
 	$args['category_name'] = 'Primary';
- 
+
 	// it is always better to use WP_Query but not here
 	query_posts( $args );
- 
+
 	if( have_posts() ) :
- 
+
 		// run the loop
 		while ( have_posts() ) : the_post();
 
 				get_template_part( 'template-parts/content-blogfeed', get_post_format() );
 
 			endwhile;
- 
+
 	endif;
 	die; // here we exit the script and even no wp_reset_query() required!
 }
- 
- 
- 
+
+
+
 add_action('wp_ajax_loadmore', 'misha_loadmore_ajax_handler'); // wp_ajax_{action}
 add_action('wp_ajax_nopriv_loadmore', 'misha_loadmore_ajax_handler'); // wp_ajax_nopriv_{action}
 
@@ -431,7 +432,7 @@ function create_product_taxonomies() {
 // Options Page
 
 if( function_exists('acf_add_options_page') ) {
-	
+
 	acf_add_options_page(array(
 		'page_title' 	=> 'Footer Info',
 		'menu_title'	=> 'Footer Info',
@@ -439,7 +440,7 @@ if( function_exists('acf_add_options_page') ) {
 		'capability'	=> 'edit_posts',
 		'redirect'		=> false
 	));
-	
+
 }
 
 add_theme_support('auto-load-next-post');
