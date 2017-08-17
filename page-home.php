ef2e5732e56ced6fb916e0674d52d36733452134
Sean Brodbeck <sean@seanbrodbeck.com>
2017-08-17 09:53:19 -0400

    Home Perspectives External Link Update and Laygridder Large Padding Overrider Fix

--- page-home.php
+++ page-home.php
@@ -151,28 +151,40 @@ get_header('home'); ?>
 									$myposts = get_posts( $args );
 									foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
 										<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+										<?php if( get_field('post_link_external') ): ?>
+											<a href="<?php the_field('post_link_external'); ?>"><?php the_post_thumbnail("full"); ?></a>
+										<?php else: ?>
 											<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail("full"); ?></a>
-											<header class="entry-header">
-												<div class="category-list">
-													<ul>
-														<?php
-															$categories = get_the_category();
-															$separator = ' · ';
-															$output = '';
-															if($categories){
-															    foreach($categories as $category) {
-															if($category->name !== 'Primary'){
-															        $output .= '<li>'.$category->cat_name.'</li>'.$separator;}
-															    }
-															echo trim($output, $separator);
-															}
-														?>
-													</ul>
+										<?php endif; ?>
+											<div class="entry-text">
+												<header class="entry-header">
+													<div class="category-list">
+														<ul>
+															<?php
+																$categories = get_the_category();
+																$separator = ' · ';
+																$output = '';
+																if($categories){
+																    foreach($categories as $category) {
+																if($category->name !== 'Primary'){
+																        $output .= '<li>'.$category->cat_name.'</li>'.$separator;}
+																    }
+																echo trim($output, $separator);
+																}
+															?>
+														</ul>
+													</div>
+													<?php if( get_field('post_link_external') ): ?>
+													<h2><a href="<?php the_field('post_link_external'); ?>"><span><?php the_field('perspectives_source'); ?></span> <?php the_title(); ?></a></h2>
+													<?php else: ?>
+													<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+													<?php endif; ?>
+												</header>
+												<div class="entry-content">
+													<?php //the_excerpt(); ?>
+													<p><?php the_field('intro_paragraph_text'); ?></p>
 												</div>
-												<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-											</header>
-											<div class="entry-content">
-												<?php the_excerpt(); ?>
 											</div>
 										</article>
 									<?php endforeach;
@@ -188,9 +200,19 @@ get_header('home'); ?>
 									$myposts = get_posts( $args );
 									foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
 										<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+										<?php if( get_field('post_link_external') ): ?>
+											<a href="<?php the_field('post_link_external'); ?>"><?php the_post_thumbnail("full"); ?></a>
+										<?php else: ?>
 											<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail("full"); ?></a>
-											<header class="entry-header">
-												<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+										<?php endif; ?>
+											<div class="entry-text">
+												<header class="entry-header">
+													<?php if( get_field('post_link_external') ): ?>
+													<h3><a href="<?php the_field('post_link_external'); ?>"><span><?php the_field('perspectives_source'); ?></span> <?php the_title(); ?></a></h2>
+													<?php else: ?>
+													<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+													<?php endif; ?>
+												</header>
 												<div class="category-list">
 													<ul>
 														<?php
@@ -207,7 +229,7 @@ get_header('home'); ?>
 														?>
 													</ul>
 												</div>
-											</header>
+											</div>
 										</article>
 									<?php endforeach;
 									wp_reset_postdata();?>
